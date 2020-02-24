5ccf176e4f8c4bfc37cfa29c17a0e997bd5f8e77
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-02-24 10:32:03 +0700

    Update File 1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,91 +1,103 @@
 package com.company;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public
+class Main
+    {
 
-    public static void main(String[] args) {
-	// write your code here
+        public static
+        void main(String[] args)
+            {
+                // write your code here
 
 //        Scanner
-        Scanner input = new Scanner(System.in);
+                Scanner input = new Scanner(System.in);
 
 //        List Menu
-        String[][] menu = {
-                {"pizza cheesy bread", "30500"},
-                {"pizza calzone", "33000"},
-                {"pizza apple strudel", "17000"}
-        };
+                String[][] menu = {
+                        {"pizza cheesy bread", "30500"},
+                        {"pizza calzone", "33000"},
+                        {"pizza apple strudel", "17000"}
+                };
 
 //        MySecondClass
-        MySecondClass.Coba("Selamat Datang Di Pizza Apa Adanya");
-        MySecondClass.Coba("Menu Yang Tersedia :");
-        MySecondClass.Coba("------------------------");
+                MySecondClass.Coba("Selamat Datang Di Pizza Apa Adanya");
+                MySecondClass.Coba("Menu Yang Tersedia :");
+                MySecondClass.Coba("------------------------");
 //        Untuk Memunculkan Menu
-        for (String[] namamakan : menu) {
-            for (String menumakan : namamakan) {
-                System.out.print(menumakan + " ");
-            }
-            System.out.println();
-        }
+                for (String[] namamakan : menu)
+                    {
+                        for (String menumakan : namamakan)
+                            {
+                                System.out.print(menumakan + " ");
+                            }
+                        System.out.println();
+                    }
 
-        MySecondClass.Coba("------------------------");
+                MySecondClass.Coba("------------------------");
 
 //        Looping
-        boolean looping = true;
-        boolean info = false;
-        int uang = 0;
-        while (looping) {
+                boolean looping = true;
+                boolean info = false;
+                int uang = 0;
+                while (looping)
+                    {
 
-            try
-                {
-                    MySecondClass.Coba("Pizza Yang Dipilih : ");
-                    String pilihan = input.nextLine().toLowerCase().trim();
-                    for (String[] makanan : menu) {
-                        for (String dimakan : makanan) {
-                            if (dimakan.equals(pilihan)) {
-                                uang = MySecondClass.KonvertInt(makanan[1].trim());
-                                info = true;
-                            }
-                        }
-                    }
-
-//                    Apabila produk yang dipilih tidak tersedia
-                    if (!info) {
-                        MySecondClass.Coba("Produk Yang Anda Pilih Tidak Ada");
-                        System.out.println("Apakah anda ingin mencari lagi ? (y/n) ");
-                        String konfirmasi = input.nextLine().toLowerCase().trim();
-                        if (konfirmasi.contains("n")) {
-                            return;
-                        }
-                        MySecondClass.Coba("Pizza Yang Dipilih : ");
-                        pilihan = input.nextLine().toLowerCase().trim();
-                        for (String[] makanan : menu)
+                        try
                             {
-                                for (String dimakan : makanan)
+                                MySecondClass.Coba("Pizza Yang Dipilih : ");
+                                String pilihan = input.nextLine().toLowerCase();
+                                for (String[] makanan : menu)
                                     {
-                                        if (dimakan.equals(pilihan))
+                                        for (String dimakan : makanan)
                                             {
-                                                uang = MySecondClass.KonvertInt(makanan[1].trim());
-                                                info = true;
+                                                if (dimakan.equals(pilihan))
+                                                    {
+                                                        uang = MySecondClass.KonvertInt(makanan[1].trim());
+                                                        info = true;
+                                                    }
                                             }
                                     }
+
+//                    Apabila produk yang dipilih tidak tersedia
+                                if (!info)
+                                    {
+                                        MySecondClass.Coba("Produk Yang Anda Pilih Tidak Ada");
+                                        System.out.println("Apakah anda ingin mencari lagi ? (y/n) ");
+                                        String konfirmasi = input.nextLine().toLowerCase();
+                                        if (konfirmasi.contains("n"))
+                                            {
+                                                return;
+                                            }
+                                        MySecondClass.Coba("Pizza Yang Dipilih : ");
+                                        pilihan = input.nextLine().toLowerCase();
+                                        for (String[] makanan : menu)
+                                            {
+                                                for (String dimakan : makanan)
+                                                    {
+                                                        if (dimakan.equals(pilihan))
+                                                            {
+                                                                uang = MySecondClass.KonvertInt(makanan[1]);
+                                                                info = true;
+                                                            }
+                                                    }
+                                            }
+                                    }
+
+                                MySecondClass.Coba("Total Harga : " + MySecondClass.KonvertString(uang));
+                                MySecondClass.Coba("Masukkan Uang Anda : ");
+                                int uanganda = input.nextInt();
+                                int samadengan = uanganda - uang;
+                                MySecondClass.Coba("Kembalian : " + MySecondClass.KonvertString(samadengan));
+                                break;
+                            } catch (Exception e)
+                            {
+                                e.printStackTrace();
                             }
+
                     }
 
-                    MySecondClass.Coba("Total Harga : " + MySecondClass.KonvertString(uang));
-                    MySecondClass.Coba("Masukkan Uang Anda : ");
-                    int uanganda = input.nextInt();
-                    int samadengan = uanganda - uang;
-                    MySecondClass.Coba("Kembalian : " + MySecondClass.KonvertString(samadengan));
-                    break;
-                }
-            catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-
-        }
-
+            }
     }
-}
