d2c072dfe3bad58fdd25387bdbe6c9345949ab6a
Muhammad Farras Jibran <farasjibran@gmail.com>
2020-02-24 10:23:15 +0700

    Update File 1.0

---
+++ src/com/company/MySecondClass.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class MySecondClass {
+
+//    Print String
+    public static void Coba(String kata) {
+        System.out.println(kata);
+    }
+
+//    Convert String To Integer
+    public static int KonvertInt(String angka) {
+        return Integer.parseInt(angka);
+    }
+
+//    Convert Integer To String
+    public static String KonvertString(int masukkan) {
+        return String.valueOf(masukkan);
+    }
+
+    }
